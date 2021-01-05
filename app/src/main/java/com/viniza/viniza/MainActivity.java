428b8bb6fa66c9f5cd38533ae17d89f1d08038f2
Antonio <jalvarezn1400@alumno.ipn.mx>
2021-01-04 20:49:31 -0600

    Initial commit

---
+++ app/src/main/java/com/viniza/viniza/MainActivity.java
@@ -0,0 +1,14 @@
+package com.viniza.viniza;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
